71a4d468f2b4c3ba528caddedf6fcc8e61bf24b7
Ted Collins <theodore.collins2023@outlook.com>
2024-05-09 10:41:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Assignment1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point P1 = new Point();
+            Point P2 = new Point();
+
+            if (P1.x == P2.x)
+            {
+                Console.WriteLine("Points are on the same axis.");
+            }
+            else 
+            { 
+                Console.WriteLine("Points are not on the same axis.");
+            }
+            
+        }
+
+        public struct Point
+        {
+            public double x;
+            public double y;
+
+        }
+
+    }
+}
